5ec8a97aac18302683079f4cec17bad85c9f1940
Mrigank Pawagi <mrigankpawagi@gmail.com>
2025-03-09 14:07:03 +0530

    include mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -7,6 +7,9 @@ package «LeanearTemporalLogic» where
 lean_lib «LeanearTemporalLogic» where
   -- add library configuration options here
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_exe «leaneartemporallogic» where
   root := `Main
